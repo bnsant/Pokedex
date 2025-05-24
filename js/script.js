66c3dc96c89ddba29979ecc45a01d8337129a08c
bnsant <bernardosanlima@gmail.com>
2025-05-24 00:05:02 -0300

    script features

--- js/script.js
+++ js/script.js
@@ -1,21 +1,56 @@
-const pokemonName = document.querySelector('.pokemon__name');
-const pokemonNumber = document.querySelector(".pokemon__number");
-const pokemonImage = document.querySelector(".pokemon__image");
+const pokemonName = document.querySelector('.pokemon__name')
+const pokemonNumber = document.querySelector('.pokemon__number')
+const pokemonImage = document.querySelector('.pokemon__image')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.search__input')
+const buttonPrev = document.querySelector(".btn-prev")
+const buttonNext = document.querySelector(".btn-next")
+
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
- const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
- const data = await APIResponse.json(); 
- return data;
-
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  
+  if (APIResponse.status === 200) {
+   const data = await APIResponse.json();
+   return data;
+  }
 }
 
 const renderPokemon = async (pokemon) => {
 
-  const data = await fetchPokemon(pokemon);
+  pokemonName.innerHTML = 'Loading...'
+  pokemonNumber.innerHTML = ''
 
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
-  pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-} 
+  const data = await fetchPokemon(pokemon)
 
-renderPokemon('254');
+if (data) {
+  pokemonName.innerHTML = data.name
+  pokemonNumber.innerHTML = data.id
+  pokemonImage.src =
+    data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+      "front_default"
+    ]
+    input.value = '';
+  }else {
+  pokemonName.innerHTML = 'Not Found :c'
+  pokemonNumber.innerHTML = ''
+  }
+}
+
+form.addEventListener('submit', (event) => { 
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+  input.value = '';
+}); 
+
+buttonPrev.addEventListener('click', () => {
+  alert('prev clicked')
+});
+
+buttonNext.addEventListener('click', () => {
+  alert('next clicked')
+});
+renderPokemon(searchPokemon);
